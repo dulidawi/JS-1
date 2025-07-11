4ce6632abc5cd86ad1dfe6a76ed9f21189e6b04e
Oleksandra <157833785+dulidawi@users.noreply.github.com>
2025-07-11 18:26:55 +0200

    123

---
+++ js/lesson.js
@@ -0,0 +1,57 @@
+const age = 17;
+console.log(age);
+
+const myName = "Oleksandra";
+console.log(myName);
+
+const isStudent = confirm("Are you a student?")
+console.log(isStudent);
+
+const myString = "Never give up!"
+console.log(myString);
+
+let myNumber = 23;
+myNumber = myNumber + 10;
+console.log(myNumber);
+
+let myNull = null
+console.log(myNull);
+
+const userName = prompt("Введіть своє ім’я")
+alert(userName);
+console.log(typeof userName);
+
+const userAnswer = confirm("Ви дійсно хочете покинути сторінку?")
+console.log(userAnswer);
+
+const message = "Увага! Дія може бути небезпечною."
+alert(message)
+const answerMassage = confirm("Ви впевнені, що хочете продовжити?")
+
+
+
+
+
+
+
+
+
+
+
+// const userName = "Mango";
+// console.log("User name is", userName);
+
+// const text = "Have a nice day!";
+// alert(text);
+
+// const question = confirm ("Are you agree?");
+// console.log(question);
+
+// const asking = confirm("Are you sure?")
+// console.log(asking);
+
+// const age = prompt("How old are you?")
+// console.log(age);
+
+
+
